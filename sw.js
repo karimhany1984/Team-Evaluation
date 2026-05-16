8d5355a4bf97b03f849949ca7a91c197f288f1fa
karimhany1984 <137601144+karimhany1984@users.noreply.github.com>
2026-05-16 13:11:27 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,8 +6,7 @@ const PRE_CACHE_ASSETS = [
     BASE + 'index.html',
     BASE + 'manifest.json',
     BASE + 'icon.png',
-    BASE + 'template.docx',
-    BASE + 'template.xlsx'
+   
 ];
 
 // Install: Cache essential files with error handling
