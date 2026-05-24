2daca2c31ca8cfe09c24402a29e2649cc4ffb8c8
karimhany1984 <137601144+karimhany1984@users.noreply.github.com>
2026-05-24 13:55:42 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'trial-dynamic-v50';
+const CACHE_NAME = 'trial-dynamic-v51';
 const BASE = '/trial/';  // Match your GitHub Pages repo name
 
 // Files that MUST be available offline immediately
